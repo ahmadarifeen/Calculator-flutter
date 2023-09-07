588fb8e68aab934c01309cb1677c9f5563ce2ae6
ahmadarifeen <1831066@iub.edu.bd>
2023-09-07 13:08:51 +0600

    Push

--- lib/main.dart
+++ lib/main.dart
@@ -19,26 +19,38 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
-  String _expression = '';
-  String _result = '0'; // Initialize the result as '0'
+  String _expression = '0';
+  String _result = '';
   Color _displayTextColor = Colors.black;
 
+  bool _isOpenBracket = false;
   void _onButtonPressed(String buttonText) {
     setState(() {
       if (buttonText == 'AC') {
-        _expression = '';
-        _result = '0';
+        _expression = '0';
+        _result = '';
       } else if (buttonText == '=') {
         try {
-          _result = _evaluateExpression(_expression);
+          // Remove non-numeric characters before evaluation
+          String sanitizedExpression =
+              _expression.replaceAll(RegExp(r'[^0-9+\-*/().]'), '');
+          _result = _evaluateExpression(sanitizedExpression);
           _expression = _result;
           _displayTextColor = Colors.black;
         } catch (e) {
           _result = 'Error';
         }
+      } else if (buttonText == '()') {
+        if (_isOpenBracket) {
+          _expression += ')';
+          _isOpenBracket = false;
+        } else {
+          _expression += '(';
+          _isOpenBracket = true;
+        }
       } else {
-        if (_result == '0') {
-          _result = '';
+        if (_expression == '0' && buttonText != '.') {
+          _expression = '';
         }
         _expression += buttonText;
         _displayTextColor = Colors.black;
@@ -57,7 +69,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     }
   }
 
-  Widget _buildButton(String buttonText) {
+  Widget _buildButton(String buttonText, {bool isBracket = false}) {
     Color bgColor = Colors.white;
     Color textColor = Colors.black;
 
@@ -65,13 +77,13 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       child: Container(
         margin: EdgeInsets.all(4.0),
         child: ElevatedButton(
-          onPressed: () => _onButtonPressed(buttonText),
+          onPressed: () => _onButtonPressed(isBracket ? '()' : buttonText),
           child: Text(
-            '$buttonText',
+            isBracket ? '()' : '$buttonText',
             style: TextStyle(fontSize: 35, color: textColor),
           ),
           style: ElevatedButton.styleFrom(
-            shape: CircleBorder(),
+            shape: isBracket ? CircleBorder() : StadiumBorder(),
             primary: bgColor,
             padding: EdgeInsets.all(20),
           ),
@@ -85,98 +97,80 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculator'),
-        centerTitle: true, // Center the title text in the app bar
+        centerTitle: true,
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 5),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            // Calculator display
-            SingleChildScrollView(
-              scrollDirection:
-                  Axis.horizontal, // Set scroll direction to horizontal
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Text(
-                      _result.isEmpty
-                          ? '$_expression'
-                          : '$_expression = $_result',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: _displayTextColor,
-                        fontSize: 100,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _buildButton('AC'), // 'AC' button
-                _buildButton('+/-'),
-                _buildButton('%'),
-                _buildButton('/'),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _buildButton('7'),
-                _buildButton('8'),
-                _buildButton('9'),
-                _buildButton('*'), // Use '*' for multiplication
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _buildButton('4'),
-                _buildButton('5'),
-                _buildButton('6'),
-                _buildButton('-'),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _buildButton('1'),
-                _buildButton('2'),
-                _buildButton('3'),
-                _buildButton('+'),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: () {
-                    _onButtonPressed('0');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    primary: Colors.white,
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
                   child: Text(
-                    '0',
-                    style: TextStyle(fontSize: 35, color: Colors.black),
+                    _expression,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: _displayTextColor,
+                      fontSize: 100,
+                    ),
                   ),
                 ),
-                _buildButton('.'),
-                _buildButton('='),
-              ],
-            ),
-            SizedBox(height: 10),
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildButton('AC'),
+                  _buildButton('+/-'),
+                  _buildButton('%'),
+                  _buildButton('/'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildButton('7'),
+                  _buildButton('8'),
+                  _buildButton('9'),
+                  _buildButton('*'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildButton('4'),
+                  _buildButton('5'),
+                  _buildButton('6'),
+                  _buildButton('-'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildButton('1'),
+                  _buildButton('2'),
+                  _buildButton('3'),
+                  _buildButton('+'),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildButton('0'),
+                  _buildButton('.'),
+                  _buildButton('()',
+                      isBracket: true), // Bracket button with custom flag
+                  _buildButton('='),
+                ],
+              ),
+              SizedBox(height: 10),
+            ],
+          ),
         ),
       ),
     );
